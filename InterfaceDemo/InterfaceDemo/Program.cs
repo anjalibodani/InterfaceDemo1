f3a9727c9e31cd7d1a6b8be5a773d90f9be3ea65
anjalibodani <64855182+anjalibodani@users.noreply.github.com>
2020-05-11 19:35:22 +0530

    Add files via upload

---
+++ InterfaceDemo/InterfaceDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+namespace Console_Application_Interface
+{
+    interface ICustomer
+    {
+        void print();
+
+    }
+    interface I2Customer : ICustomer
+    {
+        void display();
+    }
+    public class Customer : I2Customer
+    {
+        public void print()
+        {
+            Console.WriteLine("Print method of interface 1");
+            
+
+        }
+        public void display()
+        {
+            Console.WriteLine("Display method of interface 2");
+            
+        }
+
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Customer C1 = new Customer();
+            C1.print();
+            C1.display();
+
+        }
+    }
+}
